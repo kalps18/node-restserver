c7434d49f3655dbc9c50b7bbdf0c50f45b312b32
kalps18 <jcesarlopezag@hotmail.com>
2023-01-30 17:56:28 -0500

    primer commit

---
+++ controllers/usuarios.js
@@ -0,0 +1,53 @@
+const { response, request } = require('express');
+
+const usuariosGet = (req = request, res = response) => {
+    const { q, nombre = "No name", apikey, page="1", limit = "10" } = req.query;
+
+    res.json({
+        msg: 'get API - controller',
+        q, nombre, apikey, page, limit
+    })
+}
+
+const usuariosPut = (req, res = response) => {
+
+    const id = req.params.id;
+
+    res.status(500).json({
+        msg: 'put API - controller',
+        id
+    })
+}
+
+const usuariosPost = (req, res = response) => {
+
+    const { nombre, edad, id } = req.body;
+
+
+    res.status(201).json({
+        msg: 'post API - controller',
+        nombre, edad, id
+    })
+}
+
+const usuariosDelete = (req, res = response) => {
+    res.json({
+        msg: 'delete API - controller'
+
+    })
+}
+
+const usuariosPatch = (req, res = response) => {
+    res.json({
+        msg: 'delete PATCH - controller'
+
+    })
+}
+
+module.exports = {
+    usuariosGet,
+    usuariosPut,
+    usuariosPost,
+    usuariosDelete,
+    usuariosPatch
+}
